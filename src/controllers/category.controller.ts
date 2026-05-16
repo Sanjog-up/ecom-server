d9e54a61119ccc3798fbc53a9ec89fd43dc7d9e3
Sanjog Aale <sanjogm940@gmail.com>
2026-05-16 23:46:05 +0545

    category models

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { Category } from "..models/category.model";
+import { Category } from "..category.model";
 
 
 //! get all
--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -1,6 +1,26 @@
-// name: require, description
+import mongoose from "mongoose";
 
-// cat schema
+//! category schema
+const categorySchema = new mongoose.Schema(
+{
+    // name: require, description
+    name: {
+        type: String,
+        required: [true, "name is required"],
+        minLength: [3, "Category name must be 3 characters long"],
+        trim: true,
+    },
+    
+        description: {
+            type: String,
+            trim: true,
+        }
+    }, 
+    {timestamps: true },
+);
 
-// model
+//! model
+const Category = mongoose.model("category", categorySchema);
+
+export default Category;
 
