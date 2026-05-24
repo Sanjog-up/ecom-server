c3056e982588eab4f8ba26c78b527c2a77a5a491
Sanjog Aale <sanjogm940@gmail.com>
2026-05-25 00:00:16 +0545

    add: auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,12 +1,28 @@
-import { NextFunction } from "express"
-import AppError from "../utils/appError.utils"
-
+import { NextFunction } from "express";
+import AppError from "../utils/appError.utils";
+import { verifyToken }  from "../utils/jwt.utilis";
+import ENV_CONFIG from "../config/env.config";
+import { Role } from "../types/enum.types";
 
 
 export const authenticate = () =>{
     return async(req: Request, res: Response, next: NextFunction) => {
         try {
+            //! get token from req cookie
+            const cookies = req.cookies;
+            const access_token = cookies["access_token"]; 
+            console.log(access_token);
+
+            if(!access_token){
+                throw new AppError("Unauthorized. Access denied", 401);
+            }
+
+            //! verify
+            const decode_data: any = verifyToken(access_token);
             
+            if(!access_token){
+                throw new AppError("Unauthorized. Access denied", 401);
+            }
         } catch (error) {
             next(new AppError("Something went wrong", 500));
         }
