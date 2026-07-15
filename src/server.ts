9b7865e59bb712947abccb92c32e51d2b8f5fa54
Sanjog Aale <sanjogm940@gmail.com>
2026-07-16 03:22:32 +0545

    DB_URI updated

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,13 @@ import ENV_CONFIG from "./config/env.config";
 
 const PORT = process.env.PORT;
 
-connectDatabase(ENV_CONFIG.db_uri);
-
-
-//! listening on port
-app.listen(PORT, () => {
+connectDatabase(ENV_CONFIG.db_uri)
+.then(()=> {
+  //! listening on port
+  app.listen(PORT, () => {
   console.log(`server is running at http://localhost:${PORT}`);
 });
+})
+.catch((err)=> {
+  console.log(`Failed to connect to databse, existing:`, err)
+})
