911aed6fe9379a831f1c1445d830ebecd4bd46eb
Sanjog Aale <sanjogm940@gmail.com>
2026-07-16 03:16:26 +0545

    changed BR_URI

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import app from "./app";
 import connectDatabase from "./config/db.config";
-const DB_URI = "mongodb://localhost/team_12";
+import ENV_CONFIG from "./config/env.config";
 
 const PORT = process.env.PORT;
 
-connectDatabase(DB_URI);
+connectDatabase(ENV_CONFIG.db_uri);
+
 
 //! listening on port
 app.listen(PORT, () => {
